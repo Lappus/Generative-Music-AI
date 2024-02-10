68dba79c219a34a18bf49aecef5603a685d2a524
Lappus <juli.cornelsen@googlemail.com>
2024-02-10 13:44:07 +0100

    just cutting unnecessary stuf out

--- NewGANsForMnist.py
+++ NewGANsForMnist.py
@@ -196,41 +196,6 @@ test_log_dir = 'logs/test'
 train_summary_writer = tf.summary.create_file_writer(train_log_dir)
 test_summary_writer = tf.summary.create_file_writer(test_log_dir)
 
-'''@tf.function
-def train_step(images):
-    noise = tf.random.normal([BATCH_SIZE, noise_dim])
-
-    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
-        generated_images = generator(noise, training=True)
-
-        real_output = discriminator(images, training=True)
-       fake_output = discriminator(generated_images, training=True)
-
-        gen_loss = generator_loss(fake_output)
-        disc_loss = discriminator_loss(real_output, fake_output)
-    
-    gradients_of_gen = gen_tape.gradient(gen_loss, generator.trainable_variables)
-    gradients_of_disc = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
-
-    generator_optimizer.apply_gradients(zip(gradients_of_gen, generator.trainable_variables))
-    discriminator_optimizer.apply_gradients(zip(gradients_of_disc, discriminator.trainable_variables))
-
-def train(dataset, epochs):
-    for epoch in range(epochs):
-        start = time.time()
-
-        for image_batch in dataset:
-            train_step(image_batch)
-        
-        generate_and_save_images(generator, epoch+1, seed)
-
-        if (epoch + 1) % 10 == 0:
-            checkpoint.save(file_prefix=checkpoint_prefix)
-        print('Time for epoch {} is {} sec'.format(epoch+1, time.time()-start))
-    
-    generate_and_save_images(generator, epochs, seed)
-'''
-seed = tf.random.normal([num_examples_to_generate, noise_dim])
 
 checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
 
